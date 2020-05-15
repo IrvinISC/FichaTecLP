772c009f627c5c01168cf820b9b641b32a7d2b99
IrvinISC <49654828+IrvinISC@users.noreply.github.com>
2020-05-14 20:24:20 -0500

    Se agrego la parte 3 del formulario
    se adapto la parte 3 al formulario de la ficha y se corrigio la transicion para que funcione mejor

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const SigButton = document.getElementById('Siguiente');
+const UnoButton = document.getElementById('Uno');
+const DosButton = document.getElementById('Dos');
+const TresButton = document.getElementById('Tres');
 const AntButton = document.getElementById('Anterior');
 const container = document.getElementById('container');
 
-SigButton.addEventListener('click', () => {
+/*SigButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
 AntButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
-});
+});*/
+function uno(){
+	container.classList.remove("right-panel-active-2");
+	container.classList.remove("right-panel-active-1");
+}
+function dos(){
+	container.classList.remove("right-panel-active-2");
+	container.classList.add("right-panel-active-1");
+}
+function tres(){
+	container.classList.add("right-panel-active-2");
+}
