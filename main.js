83491b14d66d285eba0dfecc8eb473603266c583
IrvinISC <49654828+IrvinISC@users.noreply.github.com>
2020-05-26 13:12:18 -0500

    cambios en JS
    se borraron las variables y funciones no utilizadas en Js

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-const SigButton = document.getElementById('Siguiente');
-const UnoButton = document.getElementById('Uno');
-const DosButton = document.getElementById('Dos');
-const TresButton = document.getElementById('Tres');
-const AntButton = document.getElementById('Anterior');
 const container = document.getElementById('container');
-
-/*SigButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-AntButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});*/
 function uno(){
 	container.classList.remove("right-panel-active-3");
 	container.classList.remove("right-panel-active-2");
