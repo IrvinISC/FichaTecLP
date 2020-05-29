a2cb00a3a0edc453a0fff81520cb905c96ff22e7
IrvinISC <49654828+IrvinISC@users.noreply.github.com>
2020-05-29 18:05:01 -0500

    Anmaciones
    se agrego funcionalidad a los botones anterior y siguiente

--- src/public/animations.js
+++ src/public/animations.js
@@ -1,18 +1,45 @@
 const container = document.getElementById('container');
+var b = 1;
 function uno(){
+	b = 1;
 	container.classList.remove("right-panel-active-3");
 	container.classList.remove("right-panel-active-2");
 	container.classList.remove("right-panel-active-1");
 }
 function dos(){
+	b = 2;
 	container.classList.remove("right-panel-active-3");
 	container.classList.remove("right-panel-active-2");
 	container.classList.add("right-panel-active-1");
 }
 function tres(){
+	 b = 3;
 	container.classList.remove("right-panel-active-3");
 	container.classList.add("right-panel-active-2");
 }
 function cuatro(){
+	b = 4;
 	container.classList.add("right-panel-active-3");
+}
+function siguiente(){
+	if(b == 1){
+		dos();
+	}else if(b == 2){
+		tres();
+	}else if(b == 3){
+		cuatro();
+	}else{
+		uno();
+	}
+}
+function anterior(){
+	if(b == 4){
+		tres();
+	}else if(b == 3){
+		dos();
+	}else if(b == 2){
+		uno();
+	}else{
+		cuatro();
+	}
 }
