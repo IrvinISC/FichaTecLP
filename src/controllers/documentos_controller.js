04929327ae4d6ebc720b20647df9c4167704fd1f
IrvinISC <49654828+IrvinISC@users.noreply.github.com>
2020-05-29 15:56:24 -0500

    Update documentos_controller.js

--- src/controllers/documentos_controller.js
+++ src/controllers/documentos_controller.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary');
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.envCLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 const fs = require('fs-extra');
 
